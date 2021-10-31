eba1d84e599d6bf60532abf93abd50657296df85
msp4msps <msp4msps@tminus365.com>
2021-10-31 11:57:30 -0600

    File paths and initial engineer html template

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
