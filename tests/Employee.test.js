c7b6fa2b29154fe9851f960f9e4b619a12dcfcd3
msp4msps <msp4msps@tminus365.com>
2021-10-31 10:49:44 -0600

    Add Jest and first Employee test file

---
+++ tests/Employee.test.js
@@ -0,0 +1,42 @@
+const Employee = require("./Employee");
+
+describe("Can instanciate Employeee instance and call all parameters/functions", () => {
+  it("can instanciate Employeee Instance", () => {
+    const NewEmployee = new Employee();
+    expect(typeof NewEmployee).toBe("object");
+  });
+  it("can set name of employee", () => {
+    const name = "Nick";
+    const NewEmployee = new Employee(name);
+    expect(NewEmployee.name).toBe(name);
+  });
+  it("can set id of employee", () => {
+    const id = "1";
+    const NewEmployee = new Employee("Nick", id);
+    expect(NewEmployee.id).toBe(id);
+  });
+  it("can set email of employee", () => {
+    const email = "Nick@gmail.com";
+    const NewEmployee = new Employee("Nick", 1, email);
+    expect(NewEmployee.email).toBe(email);
+  });
+  it("can get name of employee", () => {
+    const name = "Nick";
+    const NewEmployee = new Employee(name);
+    expect(NewEmployee.getname()).toBe(name);
+  });
+  it("can get Id of employee", () => {
+    const Id = "2";
+    const NewEmployee = new Employee("Nick", Id);
+    expect(NewEmployee.getId()).toBe(Id);
+  });
+  it("can get Email of employee", () => {
+    const Email = "Nick@gmail.com";
+    const NewEmployee = new Employee("nick", 1, Email);
+    expect(NewEmployee.getEmail()).toBe(Email);
+  });
+  it("can get role of employee", () => {
+    const NewEmployee = new Employee("nick", 1, Email);
+    expect(NewEmployee.getRole()).toBe("Employee");
+  });
+});
