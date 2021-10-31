8a7df323aa61781e4677028d73e4001424b08cd6
msp4msps <msp4msps@tminus365.com>
2021-10-31 10:50:39 -0600

    Update to required file path

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,4 +1,4 @@
-const Employee = require("./Employee");
+const Employee = require("../lib/Employee");
 
 describe("Can instanciate Employeee instance and call all parameters/functions", () => {
   it("can instanciate Employeee Instance", () => {
